6e9a0182f37afd378ec73e5eff2e15af6e06593a
Stamatis Papadopoulos <stampap1997@gmail.com>
2021-05-01 17:47:00 +0300

    added server and database

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+var cors = require('cors')
+app.use(cors())
+const dotenv=require("dotenv");
+dotenv.config()
+const Port = process.env.Port || 8080;
+app.listen(Port, ()=>console.log("server has started!"));
+app.get("/Stamatis",(req,res)=>{res.send("it works!");});
+mongoose.connect(process.env.mongodb_access,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+error=>{
+    if (error) return console.log("error")
+    console.log("connected to mongodb!")
+})
+
+// Su6GhnY79Jpn3BvE StamPap97
+
+// mongodb+srv://StamPap97:<password>@cluster0.gkcmr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
