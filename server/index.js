5faea06ed78327708be44100dfedef4d58663e8a
Stamatis Papadopoulos <stampap1997@gmail.com>
2021-05-06 15:03:25 +0300

    added dist

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-	res.sendFile('./MuseumProject/index.html');
+	res.sendFile(path.join(__dirname, 'dist/MuseumProject/index.html'));
 });
 
 
