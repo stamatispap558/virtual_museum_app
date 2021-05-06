bb6c0ca4d477da782f9f7bf983ef7889b9a780ad
Stamatis Papadopoulos <stampap1997@gmail.com>
2021-05-06 14:40:16 +0300

    added sth new

--- server/index.js
+++ server/index.js
@@ -26,8 +26,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/../client/build')));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
