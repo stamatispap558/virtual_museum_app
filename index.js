19f035f7ab97adc83ca43e39d52247f66f96c47a
Stamatis Papadopoulos <stampap1997@gmail.com>
2021-05-06 18:49:58 +0300

    added sthsthsth

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ error=>{
 })
 
 if (process.env.NODE_ENV === 'production') {
-	app.use('/static',express.static(path.join(__dirname, "public")));
+	app.use(express.static(path.join(__dirname, "/static")));
 }
 
 app.get('/', (request, response) => {
