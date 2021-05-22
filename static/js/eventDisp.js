3a7d50c159ff2481188074c6b44d912e0f03c074
BlueSpirit48 <hcon241@gmail.com>
2021-05-22 16:49:38 +0300

    event_change

---
+++ static/js/eventDisp.js
@@ -0,0 +1,21 @@
+const eventTitle = document.querySelector('.eveTitle')
+const eventImg = document.querySelector('.eveimg')
+const eventText = document.querySelector('.text')
+
+
+fetch('/html/event.html/data')
+    .then(response => response.json())
+    .then(data =>{
+        let eventObj = data[0]
+        let elTitle = document.createElement('h2')
+        eventTitle.appendChild(elTitle);
+        elTitle.innerHTML = eventObj.title;
+        let elImg = document.createElement('img');
+        eventImg.appendChild(elImg);
+        elImg.setAttribute('src',eventObj.img)
+        let elText = document.createElement('p')
+        eventText.appendChild(elText)
+        elText.innerHTML = eventObj.text.toString()
+        //console.log(eventObj)
+        
+    })
