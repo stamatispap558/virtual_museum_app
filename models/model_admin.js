21176ca7ca5c9eb74d3212ffc8abe3c3b0e9ad69
BlueSpirit48 <hcon241@gmail.com>
2021-05-24 17:57:27 +0300

    previous_admin

--- models/model_admin.js
+++ models/model_admin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const adminSchema = new mongoose.Schema({
   Admin_Id:  {
@@ -18,7 +19,7 @@ const adminSchema = new mongoose.Schema({
   Email: {
     type:String,  
     required:true,
-  unique:true} ,
+    unique:true} ,
   phone:{
     type:Number,  
     required:true} ,
@@ -27,9 +28,30 @@ const adminSchema = new mongoose.Schema({
       require:true,
       min:[8,"Must be more than 8 characters"] 
   }
-  
+
 });
-//adminSchema.plugin(passportLocalMongoose);
+
+adminSchema.plugin(passportLocalMongoose);
 const admin=mongoose.model("admins",adminSchema);
 module.exports=admin;
 
+const express = require("express");
+const app = express();
+app.route("/add").post(function(req, res) {
+    var object = {
+      Admin_Id: "FirstId",
+      name: "Konstantinos",
+      last_name: "Palios",
+      Email: "konpalios@gmail.com",
+      phone:6943910197
+    };
+
+    admin.create(object, function(err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        console.log(result);
+        res.send(result);
+      }
+    });
+  });
